beefb6525ea474ecf633167eb24ae212a40434c1
Nathasha <nsoares@corpcloud.com.au>
2022-12-21 17:18:09 +0800

    Formatted two main cards

--- src/Content.js
+++ src/Content.js
@@ -10,6 +10,8 @@ import {
   Button,
 } from "@mui/material";
 
+import AddCircleRoundedIcon from "@mui/icons-material/AddCircleRounded";
+
 import "./App.css";
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -30,7 +32,7 @@ export function Content() {
         mr: 0,
         mb: 2,
         ml: 0,
-        bgcolor: "green",
+        bgcolor: "#F1EFF2",
         display: "inline-flex",
         flexDirection: "row",
         alignContent: "flex-start",
@@ -40,9 +42,8 @@ export function Content() {
         <Card
           sx={{
             height: "50px",
-            bgcolor: "pink",
+            bgcolor: "transparent",
             boxShadow: "none",
-            width: "100%",
           }}
         >
           <Grid
@@ -63,7 +64,6 @@ export function Content() {
           >
             Quote 1
           </Grid>
-
           <Grid
             item
             xs={2}
@@ -101,9 +101,34 @@ export function Content() {
 
           <Grid
             item
-            xs={2}
+            xs={12}
+            sx={{
+              bgcolor: "transparent",
+              display: "inline-flex",
+              justifyContent: "flex-end",
+            }}
+          >
+            <TextField
+              id="outlined-search"
+              label="Search quotes"
+              type="search"
+              size="small"
+              sx={{
+                width: "40vh",
+                height: 46,
+                p: "4px",
+                ml: 46,
+
+                alignContent: "right",
+              }}
+            />
+          </Grid>
+
+          <Button
+            variant="contained"
             sx={{
               display: "inline-flex",
+              justifyContent: "flex-end",
               border: 1,
               p: 0.5,
               borderRadius: 2,
@@ -112,103 +137,97 @@ export function Content() {
               boxShadow: 5,
               bgcolor: "#FFC20E",
               color: "#001864",
-              mb: 10,
+              textTransform: "capitalize",
+              ml: 42,
+              "&:hover": {
+                backgroundColor: "#e4c77e",
+                borderColor: "#001864",
+              },
             }}
           >
-            Quote 1
+            New quote
+          </Button>
+        </Card>
+      </Grid>
+
+      <Card
+        sx={{
+          ml: 3,
+          bgcolor: "yellow",
+          flexGrow: 2,
+        }}
+      >
+        <Grid container sx={{ color: "#001864", p: 1 }}>
+          <Grid
+            item
+            xs={2}
+            sx={{
+              display: "inline-flex",
+              p: 0.5,
+              m: 1,
+              color: "#001864",
+              fontSize: 20,
+              fontWeight: 700,
+            }}
+          >
+            Quote #0001
           </Grid>
 
           <Grid
             item
-            xs={12}
+            xs={4}
             sx={{
               bgcolor: "transparent",
               display: "inline-flex",
-              justifyContent: "flex-end",
-              alignItems: "right",
+              justifyContent: "center",
             }}
-          > 
+          >
             <TextField
               id="outlined-search"
-              label="Search quotes"
+              label="Quote's title"
               type="search"
               size="small"
               sx={{
                 width: "40vh",
                 height: 36,
                 p: "4px",
-                ml: 41,
-                alignContent: "right",
-                alignItems: "right",
+                m: 0,
+                alignContent: "center",
               }}
-            
             />
-            
           </Grid>
-        </Card>
-      </Grid>
 
-      <Card
-        sx={{
-          mr: 3,
-          ml: 3,
-          bgcolor: "yellow",
-          flexGrow: 6,
-        }}
-      >
-        <Grid
-          item
-          xs={2}
-          sx={{
-            display: "inline-flex",
-            p: 0.5,
-            m: 1,
-            color: "#001864",
-            fontWeight: 700,
-          }}
-        >
-          Quote #0001
-        </Grid>
-
-        <Grid
-          item
-          xs={6}
-          sx={{
-            bgcolor: "transparent",
-            display: "inline-flex",
-            justifyContent: "flex-end",
-          }}
-        >
-          <TextField
-            id="outlined-search"
-            label="Quote's title"
-            type="search"
-            size="small"
+          <Grid
+            item
+            xs={3}
             sx={{
-              width: "40vh",
-              height: 36,
-              p: "4px",
-              m: 0,
-              alignContent: "right",
-              alignItems: "right",
+              display: "inline-flex",
+              justifyContent: "right",
+              p: 0.5,
+              color: "#001864",
+              fontWeight: 700,
             }}
-          />
-        </Grid>
-        <Grid
-          item
-          xs={2}
-          sx={{
-            display: "inline-flex",
-            p: 0.5,
-            m: 1,
-            color: "#001864",
-          }}
-        >
-          Jonathan Matthews
+          >
+            Jonathan Matthews
+          </Grid>
+
+          <Grid
+            item
+            xs={9}
+            sx={{
+              display: "inline-flex",
+              justifyContent: "flex-end",
+              color: "#001864",
+              fontWeight: 600,
+              fontStyle: "italic",
+            }}
+          >
+            (issue date) 14/12/2022
+          </Grid>
         </Grid>
 
-        <CardContent sx={{ m: 0 }}>
-          <Grid container sx={{ color: "#fff", bgcolor: "#001864" }}>
+        <Card sx={{ mb: 10 }}>
+          <CardContent sx={{ color: "#fff", bgcolor: "#001864" }}>
             <Grid
               item
               xs={6}
@@ -226,17 +245,59 @@ export function Content() {
                 Oiiee
               </Grid>
             </Grid>
-          </Grid>
-        </CardContent>
+          </CardContent>
+        </Card>
       </Card>
       <Card
         sx={{
           mr: 3,
           ml: 3,
-          flexGrow: 6,
+          flexGrow: 9,
           bgcolor: "purple",
         }}
-      ></Card>
+      >
+        <Grid
+          item
+          xs={8}
+          sx={{
+            bgcolor: "transparent",
+            display: "inline-flex",
+            justifyContent: "flex-end",
+          }}
+        >
+          <TextField
+            id="outlined-search"
+            label="Search items"
+            type="search"
+            size="small"
+            sx={{ height: 46, p: "2px", m: 1 }}
+          />
+          <Grid
+            item
+            xs={6}
+            sx={{
+              bgcolor: "transparent",
+              display: "inline-flex",
+              justifyContent: "flex-end",
+              mt: 3,
+              ml: 5,
+              mr: 0,
+              color: "#001864",
+            }}
+          >
+            <AddCircleRoundedIcon
+              sx={{
+                mr: 1,
+                mt: -0.5,
+                bgcolor: "#001864",
+                borderRadius: 100,
+                color: "#FFC20E",
+              }}
+            />
+            Add item
+          </Grid>
+        </Grid>
+      </Card>
     </Grid>
   );
 }
