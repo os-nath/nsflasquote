2c7a8cfe9589c4c8a5f12e6f510cad50d2930615
Nathasha <nsoares@corpcloud.com.au>
2022-12-21 12:10:11 +0800

    upper menu styling

--- src/Content.js
+++ src/Content.js
@@ -1,26 +1,185 @@
 import React from "react";
-import { Card, Grid, CardContent } from "@mui/material";
+import {
+  Box,
+  Card,
+  Grid,
+  CardContent,
+  Paper,
+  styled,
+  TextField,
+} from "@mui/material";
+
+import "./App.css";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: "theme.palette.text.secondary",
+}));
 
 export function Content() {
   return (
-    <Card
+    <Grid
+      container
       sx={{
         height: 1000,
-        width: "100%",
-        m: 2,
-        bgcolor: "yellow",
+        mt: 2,
+        mr: 0,
+        mb: 2,
+        ml: 0,
+        bgcolor: "#F1EFF2",
+        display: "inline-flex",
+        flexDirection: "row",
+        alignContent: "flex-start",
       }}
     >
-      <CardContent sx={{ m: 0 }}>
-        <Grid container sx={{ color: "#fff", bgcolor: "#001864" }}>
+      <Grid xs={8} sx={{ mb: 1, ml: 3 }}>
+        <Card
+          sx={{ height: "50px", bgcolor: "transparent", boxShadow: "none" }}
+        >
+          <Grid
+            item
+            xs={2}
+            sx={{
+              display: "inline-flex",
+              border: 1,
+              p: 0.5,
+              borderRadius: 2,
+              borderColor: "#FFC20E",
+              m: 1,
+              boxShadow: 5,
+              bgcolor: "#FFC20E",
+              color: "#001864",
+              mb: 10,
+            }}
+          >
+            Quote 1
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            sx={{
+              display: "inline-flex",
+              border: 1,
+              p: 0.5,
+              borderRadius: 2,
+              borderColor: "#FFC20E",
+              m: 1,
+              boxShadow: 5,
+              bgcolor: "#FFFFFF",
+              color: "#001864",
+            }}
+          >
+            Quote 2
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            sx={{
+              display: "inline-flex",
+              border: 1,
+              p: 0.5,
+              borderRadius: 2,
+              borderColor: "#FFC20E",
+              m: 1,
+              boxShadow: 5,
+              bgcolor: "#FFFFFF",
+              color: "#001864",
+            }}
+          >
+            Quote 3
+          </Grid>
+
           <Grid
             item
             xs={6}
-            sx={{ display: "inline-flex", justifyContent: "flex-start" }}
+            sx={{
+              bgcolor: "transparent",
+              display: "inline-flex",
+              justifyContent: "flex-end",
+              alignItems: "right",
+            }}
           >
-            Oiiee
+            <TextField
+              id="outlined-search"
+              label="Type a city name"
+              type="search"
+              size="small"
+              sx={{
+                width: "40vh",
+                height: 36,
+                p: "4px",
+                m: 0,
+                alignContent: "right",
+                alignItems: "right",
+              }}
+            />
           </Grid>
+        </Card>
+      </Grid>
 
+      <Card
+        sx={{
+          mr: 3,
+          ml: 3,
+          bgcolor: "yellow",
+          flexGrow: 6,
+        }}
+      >
+        <Grid
+          item
+          xs={2}
+          sx={{
+            display: "inline-flex",
+            p: 0.5,
+            m: 1,
+            color: "#001864",
+            fontWeight: 700,
+          }}
+        >
+          Quote #0001
+        </Grid>
+
+        <Grid
+          item
+          xs={6}
+          sx={{
+            bgcolor: "transparent",
+            display: "inline-flex",
+            justifyContent: "flex-end",
+          }}
+        >
+          <TextField
+            id="outlined-search"
+            label="Type a city name"
+            type="search"
+            size="small"
+            sx={{
+              width: "40vh",
+              height: 36,
+              p: "4px",
+              m: 0,
+              alignContent: "right",
+              alignItems: "right",
+            }}
+          />
+        </Grid>
+        <Grid
+          item
+          xs={2}
+          sx={{
+            display: "inline-flex",
+            p: 0.5,
+            m: 1,
+            color: "#001864",
+          }}
+        >
+          Jonathan Matthews
+        </Grid>
+
+        <CardContent sx={{ m: 0 }}>
           <Grid container sx={{ color: "#fff", bgcolor: "#001864" }}>
             <Grid
               item
@@ -29,10 +188,28 @@ export function Content() {
             >
               Oiiee
             </Grid>
+
+            <Grid container sx={{ color: "#fff", bgcolor: "#001864" }}>
+              <Grid
+                item
+                xs={6}
+                sx={{ display: "inline-flex", justifyContent: "flex-start" }}
+              >
+                Oiiee
+              </Grid>
+            </Grid>
           </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+      <Card
+        sx={{
+          mr: 3,
+          ml: 3,
+          flexGrow: 6,
+          bgcolor: "purple",
+        }}
+      ></Card>
+    </Grid>
   );
 }
 
