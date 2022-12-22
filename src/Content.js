d26af0dca19b4736ad582f29239b8e59132cba70
Nathasha <nsoares@corpcloud.com.au>
2022-12-22 11:46:05 +0800

    Quote card header styled

--- src/Content.js
+++ src/Content.js
@@ -1,18 +1,19 @@
 import React from "react";
 import {
   Box,
+  Button,
   Card,
   Grid,
   CardContent,
+  Divider,
   Paper,
   styled,
   TextField,
-  Button,
 } from "@mui/material";
 
 import AddCircleRoundedIcon from "@mui/icons-material/AddCircleRounded";
 
-import "./App.css";
+import "./Content.css";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -153,18 +154,28 @@ export function Content() {
       <Card
         sx={{
           ml: 3,
-          bgcolor: "yellow",
+          bgcolor: "#fff",
           flexGrow: 2,
+          border: 1,
+          borderColor: "#FFC20E",
         }}
       >
-        <Grid container sx={{ color: "#001864", p: 1 }}>
+        <Grid
+          container
+          sx={{
+            color: "#001864",
+            p: 1,
+            display: "inline-flex",
+            justifyContent: "space-around",
+          }}
+        >
           <Grid
             item
             xs={2}
             sx={{
-              display: "inline-flex",
               p: 0.5,
               m: 1,
+              mr: 3,
               color: "#001864",
               fontSize: 20,
               fontWeight: 700,
@@ -179,7 +190,6 @@ export function Content() {
             sx={{
               bgcolor: "transparent",
               display: "inline-flex",
-              justifyContent: "center",
             }}
           >
             <TextField
@@ -190,24 +200,14 @@ export function Content() {
               sx={{
                 width: "40vh",
                 height: 36,
-                p: "4px",
+                p: "2px",
                 m: 0,
                 alignContent: "center",
               }}
             />
           </Grid>
 
-          <Grid
-            item
-            xs={3}
-            sx={{
-              display: "inline-flex",
-              justifyContent: "right",
-              p: 0.5,
-              color: "#001864",
-              fontWeight: 700,
-            }}
-          >
+          <Grid item xs={3} sx={{ p: 0.5, color: "#001864", fontWeight: 700 }}>
             Jonathan Matthews
           </Grid>
 
@@ -220,40 +220,79 @@ export function Content() {
               color: "#001864",
               fontWeight: 600,
               fontStyle: "italic",
+              ml: 3,
+              mt: -1,
             }}
           >
             (issue date) 14/12/2022
           </Grid>
         </Grid>
 
-        <Card sx={{ mb: 10 }}>
-          <CardContent sx={{ color: "#fff", bgcolor: "#001864" }}>
-            <Grid
-              item
-              xs={6}
-              sx={{ display: "inline-flex", justifyContent: "flex-start" }}
-            >
-              Oiiee
+        <Card sx={{ mt: 2, mb: 10, boxShadow: "none" }}>
+          <Grid
+            container
+            sx={{
+              color: "#001864",
+              fontWeight: 600,
+              display: "inline-flex",
+              justifyContent: "space-around",
+              fontSize: 18,
+              mt: 2,
+            }}
+          >
+            <Grid item xs={1} sx={{}}>
+              <AddCircleRoundedIcon
+                sx={{
+                  bgcolor: "#001864",
+                  borderRadius: 100,
+                  color: "#FFC20E",
+                }}
+              />
             </Grid>
 
-            <Grid container sx={{ color: "#fff", bgcolor: "#001864" }}>
-              <Grid
-                item
-                xs={6}
-                sx={{ display: "inline-flex", justifyContent: "flex-start" }}
-              >
-                Oiiee
-              </Grid>
+            <Grid item xs={1} sx={{ flexGrow: 2, alignSelf: "flex-start" }}>
+              Services
             </Grid>
-          </CardContent>
+
+            <Grid item xs={1} sx={{}}>
+              Supplier
+            </Grid>
+
+            <Grid item xs={1} sx={{}}>
+              Code
+            </Grid>
+
+            <Grid item xs={1} sx={{}}>
+              Qty
+            </Grid>
+
+            <Grid item xs={1} sx={{}}>
+              Cost
+            </Grid>
+
+            <Grid item xs={1} sx={{}}>
+              Sell
+            </Grid>
+          </Grid>{" "}
+          <Divider
+            sx={{
+              color: "yellowgreen",
+              bgcolor: "#FFC20E",
+              width: "92%",
+              ml: 3,
+            }}
+          />
         </Card>
       </Card>
+
       <Card
         sx={{
           mr: 3,
           ml: 3,
           flexGrow: 9,
-          bgcolor: "purple",
+          bgcolor: "#fff",
+          border: 1,
+          borderColor: "#FFC20E",
         }}
       >
         <Grid
@@ -263,6 +302,7 @@ export function Content() {
             bgcolor: "transparent",
             display: "inline-flex",
             justifyContent: "flex-end",
+            m: 1,
           }}
         >
           <TextField
@@ -274,12 +314,12 @@ export function Content() {
           />
           <Grid
             item
-            xs={6}
+            xs={7}
             sx={{
               bgcolor: "transparent",
               display: "inline-flex",
               justifyContent: "flex-end",
-              mt: 3,
+              mt: 2.5,
               ml: 5,
               mr: 0,
               color: "#001864",
